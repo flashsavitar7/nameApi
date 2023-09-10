5dc5b921261a0c0fb8fb65d1e82bfc97ec6250e7
flashsavitar7 <99418944+flashsavitar7@users.noreply.github.com>
2023-09-10 21:32:09 +0100

    add

---
+++ app.py
@@ -0,0 +1,70 @@
+from flask import Flask, request, jsonify
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://namesofpips_user:Ii8wUXW1CCnKu4KSkiTr6rm0plkGpcHM@dpg-cjv22795mpss738pq8vg-a.oregon-postgres.render.com/namesofpips'  # Replace with your actual PostgreSQL connection URI
+db = SQLAlchemy(app)
+
+class Person(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(255), nullable=False)
+
+# Create the database tables
+with app.app_context():
+    db.create_all()
+
+@app.route('/api', methods=['POST'])
+def create_person():
+    data = request.get_json()
+    name = data.get('name')
+
+    if not name:
+        return jsonify({"error": "Name is required"}), 400
+
+    person = Person(name=name)
+    db.session.add(person)
+    db.session.commit()
+
+    return jsonify({"message": "Person created successfully"}), 201
+
+@app.route('/api/<string:name>', methods=['GET'])
+def get_person_by_name(name):
+    person = Person.query.filter_by(name=name).first()
+
+    if not person:
+        return jsonify({"error": "Person not found"}), 404
+
+    return jsonify({"id": person.id, "name": person.name})
+
+@app.route('/api/<string:name>', methods=['PUT'])
+def update_person(name):
+    person = Person.query.filter_by(name=name).first()
+
+    if not person:
+        return jsonify({"error": "Person not found"}), 404
+
+    data = request.get_json()
+    new_name = data.get('name')
+
+    if not new_name:
+        return jsonify({"error": "Name is required"}), 400
+
+    person.name = new_name
+    db.session.commit()
+
+    return jsonify({"message": "Person updated successfully"})
+
+@app.route('/api/<string:name>', methods=['DELETE'])
+def delete_person(name):
+    person = Person.query.filter_by(name=name).first()
+
+    if not person:
+        return jsonify({"error": "Person not found"}), 404
+
+    db.session.delete(person)
+    db.session.commit()
+
+    return jsonify({"message": "Person deleted successfully"})
+
+if __name__ == '__main__':
+    app.run(debug=True)
