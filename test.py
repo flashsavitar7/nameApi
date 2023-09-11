01cd1e4726eec6132d68b5cf610417a7085dc7b0
triumph edet <99418944+flashsavitar7@users.noreply.github.com>
2023-09-11 20:18:31 +0100

    Update test.py

--- test.py
+++ test.py
@@ -2,23 +2,26 @@ import requests
 
 base_url = 'https://nameapi-mnmf.onrender.com/api'  # Update with your API URL
 
+# Test CREATE operation
 create_response = requests.post(base_url, json={"name": "John Doe"})
 try:
+    print("CREATE operation:")
     print(create_response.json())
 except requests.exceptions.JSONDecodeError:
     print("Non-JSON response received:")
     print(create_response.text)
 
-get_response = requests.get(f'{base_url}/John Doe')
+# Test READ operation
+get_response = requests.get(f'{base_url}/1')
 print("\nREAD operation:")
 print(get_response.json())
 
-
-update_response = requests.put(f'{base_url}/John Doe', json={"name": "Jane Doe"})
+# Test UPDATE operation
+update_response = requests.put(f'{base_url}/1', json={"name": "Jane Doe"})
 print("\nUPDATE operation:")
 print(update_response.json())
 
-
-delete_response = requests.delete(f'{base_url}/Jane Doe')
+# Test DELETE operation
+delete_response = requests.delete(f'{base_url}/1')
 print("\nDELETE operation:")
 print(delete_response.json())
